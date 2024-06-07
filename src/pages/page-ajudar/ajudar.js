c5e52dd453fcbe51c2580c10a8b5f65efe2785a4
Rodrigo Hiroshi <rodrigohiroshiwatanabe@gmail.com>
2024-06-07 16:09:20 -0300

    Window location do login

--- src/pages/page-ajudar/ajudar.js
+++ src/pages/page-ajudar/ajudar.js
@@ -4,7 +4,7 @@ function verificar(){
 
    if (emailUsuario == "1234" && senhaUsuario == 1234){
        alert("parabéns, você está logado")
-        
+        window.location = "/src/pages/home/index.html"
     }
     else{
         alert("Login ou senha está errado!")
