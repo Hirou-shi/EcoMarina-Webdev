071997a27e49527ea5fb732b7e1a7d9dac269e12
Rodrigo Hiroshi <rodrigohiroshiwatanabe@gmail.com>
2024-06-07 16:05:11 -0300

    Arrumando o bug

--- src/pages/page-ajudar/ajudar.js
+++ src/pages/page-ajudar/ajudar.js
@@ -1,9 +1,10 @@
 function verificar(){
-    var emailUsuario = document.getElementById("email");
-    var senhaUsuario = document.getElementById("senha");
+    var emailUsuario = document.getElementById("email").value;
+    var senhaUsuario = document.getElementById("senha").value;
 
-   if (emailUsuario == 1234 && senhaUsuario == 1234){
+   if (emailUsuario == "1234" && senhaUsuario == 1234){
        alert("parabéns, você está logado")
+        
     }
     else{
         alert("Login ou senha está errado!")
