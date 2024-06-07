286d74329b8d8d84d8546840348ea0d26bb03878
Rodrigo Hiroshi <rodrigohiroshiwatanabe@gmail.com>
2024-06-07 00:00:36 -0300

    A poha do carrosel

--- src/pages/home/script.js
+++ src/pages/home/script.js
@@ -16,3 +16,20 @@ function scrolltosec5() {
 function scrolltosec6() {
     document.getElementById('sec6').scrollIntoView({ behavior: 'smooth'});
 }
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function () {
+ nextImage();
+}, 5000)
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+document.getElementById("radio"+count).checked = true;
+
+}
